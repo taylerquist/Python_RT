9197dbbb7489bc8d285102cd5914d75802409f85
taylerquist <tquist@ucsc.edu>
2020-01-20 17:32:55 -0800

    Treating J as a matrix--aka a column vector

--- Cpp_version/ALI_two_stream_approx_cpp/main.cpp
+++ Cpp_version/ALI_two_stream_approx_cpp/main.cpp
@@ -396,9 +396,6 @@ int main()
     double *u_m = new double[n];     // "Minus" u interpolation coefficient array (the vector going from top down)
     double *p_m = new double[n];     // "Minus" p interpolation coefficient array (the vector going from top down)
     double *d_m = new double[n];     // "Minus" d interpolation coefficient array (the vector going from top down)
-    //double *lmbda_a = new double[n]; // Lower diagonal of the lambda_star operator
-    //double *lmbda_b = new double[n]; // Diagonal of the lambda_star operator
-    //double *lmbda_c = new double[n]; // Upper diagonal of the lambda_star operator
     double *M_a = new double[n];     // Lower diagonal of the matrix M_star
     double *M_b = new double[n];     // Diagonal of the matrix M_star
     double *M_c = new double[n];     // Upper diagonal of the matrix M_star
@@ -407,22 +404,22 @@ int main()
     double *I_plus = new double[n];  // The specific intensity for the vector going from bottom up
     double *I_minus = new double[n]; // The specific intensity for the vector going from top down
     double *S = new double[n];       // The source function
-    double *J = new double[n];       // The mean specific intensity
+    //double *J = new double[n];       // The mean specific intensity
     double *z = new double[n];       // The grid array
-    //double *ls_S = new double[n];    // Used for matrix multiplication in finding y_arr, representing lambda_star * S
 
     double **lmbda_s;                // This is the lambda_star matrix
+    double **lmbda_s_J;              // This is the matrix multiplication product of the lambda_star matrix with J
+    double **J;              // In order to do matrix multiplication, J must be a column vector
     lmbda_s = (double **) std::malloc(n * sizeof(double *));
-    for(i=0;i<n;i++)
-    {
-        lmbda_s[i] = (double *) std::malloc(n * sizeof(double));
-    }
-    double **lmbda_s_J;                // This is the lambda_star matrix
     lmbda_s_J = (double **) std::malloc(n * sizeof(double *));
+    J = (double **) std::malloc(n * sizeof(double *));
     for(i=0;i<n;i++)
     {
-        lmbda_s[i] = (double *) std::malloc(1 * sizeof(double));
+        lmbda_s[i] = (double *) std::malloc(n * sizeof(double));   // Lambda_star is an nxn matrix
+        lmbda_s_J[i] = (double *) std::malloc(1 * sizeof(double)); // Ends up being a column vector since nxn * nx1
+        J[i] = (double *) std::malloc(1 * sizeof(double)); // Column vector, nx1
     }
+
 
     // Initialize the variables
     //--------------------------------
@@ -431,8 +428,8 @@ int main()
     alpha[0] = B[i]*pow(10.,(5.-6.*z[i]));   // The emissivity, depends on z Ch.4, section 4.4.2, equation 4.32
     I_plus[0] = 1.;                          // The bottom-up specific intensity
     I_minus[0] = 0.;                         // The top-down specific intensity
-    J[0] = 1.;                               // The average specific intensity
-    S[0] = epsilon*B[0] + (1.-epsilon)*J[0]; // The source function Ch.4 section 4.4.4, equation 4.37
+    J[0][0] = 1.;                    // The average specific intensity
+    S[0] = epsilon*B[0] + (1.-epsilon)*J[0][0]; // The source function Ch.4 section 4.4.4, equation 4.37
     S_val_plus = 0.;                         // Will later be used for interpolation for the bottom-up, I+ array
     S_val_minus = 0.;                        // Will later be used for interpolation for the top-down, I-, array
 
@@ -444,8 +441,8 @@ int main()
         alpha[i] = B[i]*pow(10.,(5.-6.*z[i]));
         I_plus[i] = 0.;
         I_minus[i] = 0.;
-        J[i] = 0.;
-        S[i] = epsilon*B[i] + (1.-epsilon)*J[i];
+        J[i][0] = 0.;
+        S[i] = epsilon*B[i] + (1.-epsilon)*J[i][0];
     }
 
     // Set other grids, interpolation coefficients, and matrices
@@ -470,7 +467,7 @@ int main()
     {
         // First actually solve the matrix multiplication that will be used later to solve for the y in MX = y
         // It needs to be used in the next loop but it does not need to be re-calculated every time
-        //mat_mult(lmbda_s,J,ls_S,n);
+        mat_mult(lmbda_s,J,lmbda_s_J,n);
         for (i = 1; i < n; i++)
         {
             // First solve the bottom-up interpolated value of S
@@ -488,7 +485,7 @@ int main()
 
             // Update the mean specific intensity from both rays
             // Ch.4, section 4.4.5, equation 4.56
-            J[i] = 0.5 * (I_plus[i] + I_minus[i]);
+            J[i][0] = 0.5 * (I_plus[i] + I_minus[i]);
 
             // Solve for each component of the y array in MX = y
             // Ch.4, section 4.4.4, equation 4.48
